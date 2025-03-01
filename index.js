fef9329e0243fc3b3f8b39377809c4ae0d84fd9b
Mani <maniinnovator26@gmail.com>
2025-03-01 17:15:16 +0530

    v1.1

--- index.js
+++ index.js
@@ -15,10 +15,7 @@ app.use(bodyParser.json());
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.log(err));
 
